a0d00aba06b4ba32d6c33424376240b2a84fb74f
ramuthumu <ramu@fastmail.com>
2023-03-14 19:35:31 -0500

    add editions page to the app

--- eenadu_epaper_viewer.py
+++ eenadu_epaper_viewer.py
@@ -7,21 +7,30 @@ app = Flask(__name__)
 def get_max_date():
     url = "https://epaper.eenadu.net/Home/GetMaxdateJson"
     response = requests.get(url)
-    print(response.text)
-    return response.text
+    return response.text.strip('\"')
 
-def get_pages(date):
-    date = date.strip('\"')
-    url = f"https://epaper.eenadu.net/Home/GetAllpages?editionid=3&editiondate={date}&IsMag=0"
-    print(url)
+def get_pages(date, edition_id):
+    url = f"https://epaper.eenadu.net/Home/GetAllpages?editionid={edition_id}&editiondate={date}&IsMag=0"
     response = requests.get(url)
     pages = json.loads(response.text)
     return sorted(pages, key=lambda x: int(x['PageNo']))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+def get_editions(date):
+    url = f"https://epaper.eenadu.net/Login/GetMailEditionPages?Date={date}"
+    response = requests.get(url)
+    editions = json.loads(response.text)
+    return editions
+
+@app.route('/', methods=['GET'])
+def landing():
     max_date = get_max_date()
-    pages = get_pages(max_date)
+    editions = get_editions(max_date)
+    return render_template('landing.html', editions=editions)
+
+@app.route('/edition/<int:edition_id>', methods=['GET', 'POST'])
+def edition(edition_id):
+    max_date = get_max_date()
+    pages = get_pages(max_date, edition_id)
 
     if not pages:
         return "No pages found."
@@ -39,8 +48,7 @@ def index():
     xhighres_image_url = current_page['XHighResolution']
     overlay_image_url = xhighres_image_url.replace('.jpg', '.png')
 
-    return render_template('index.html', overlay_image_url=overlay_image_url, xhighres_image_url=xhighres_image_url, current_page_index=current_page_index, total_pages=len(pages))
+    return render_template('edition.html', overlay_image_url=overlay_image_url, xhighres_image_url=xhighres_image_url, current_page_index=current_page_index, total_pages=len(pages))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
