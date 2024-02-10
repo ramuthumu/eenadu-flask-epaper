93f630ac0f22dfaac0dd737907c0e99032580428
Ramu Thumu <ramu@fastmail.com>
2024-02-10 22:00:24 +0000

    refactor code

--- api/app.py
+++ api/app.py
@@ -36,14 +36,11 @@ def get_editions(date):
     else:
         print("Failed to fetch main editions")
         return []
-    
     # Get the district editions
     khammam_edition = get_khammam_district_editions(date)
-    
     # If Khammam edition is found, add it to the main editions list
     if khammam_edition:
         editions.append(khammam_edition)
-    
     return editions
 
 @app.route('/', methods=['GET'])
@@ -56,23 +53,18 @@ def landing():
 def edition(edition_id):
     max_date = get_max_date()
     pages = get_pages(max_date, edition_id)
-
     if not pages:
         return "No pages found."
-
     current_page_index = 0
-
     if request.method == 'POST':
         if 'next' in request.form:
             current_page_index = int(request.form['current_page_index']) + 1
         elif 'previous' in request.form:
             current_page_index = int(request.form['current_page_index']) - 1
-
     current_page_index = max(0, min(current_page_index, len(pages) - 1))
     current_page = pages[current_page_index]
     xhighres_image_url = current_page['XHighResolution']
     overlay_image_url = xhighres_image_url.replace('.jpg', '.png')
-
     return render_template('edition.html', overlay_image_url=overlay_image_url, xhighres_image_url=xhighres_image_url, current_page_index=current_page_index, total_pages=len(pages))
 
 if __name__ == '__main__':
