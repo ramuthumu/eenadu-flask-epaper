6f069d93d9ff32ed9b05dabd32e7495e28c8a397
Ramu Thumu <ramu@fastmail.com>
2024-06-04 21:26:44 -0500

    update requirements.txt

--- api/app.py
+++ api/app.py
@@ -13,16 +13,17 @@ app.config['CACHE_TYPE'] = 'SimpleCache'  # Simple in-memory cache, suitable for
 cache = Cache(app)
 cache.init_app(app)
 
-
 all_editions = {}
 
 # Configure APScheduler
 scheduler = BackgroundScheduler()
 
+
 def clear_cache():
     with app.app_context():
         cache.clear()
         print("Cache cleared.")
+
 
 # Schedule cache to be cleared every day at 5 PM and 6 PM
 scheduler.add_job(func=clear_cache, trigger='cron', hour='16,17,18')
@@ -33,10 +34,11 @@ atexit.register(lambda: scheduler.shutdown())
 
 eenadu_editions = []
 
+
 @cache.memoize(timeout=86400)  # Cache for one day
 def get_max_date_from_url(url, json_key=None):
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         response.raise_for_status()  # This will raise an exception for 4XX and 5XX responses
 
         if json_key:
@@ -50,12 +52,15 @@ def get_max_date_from_url(url, json_key=None):
         print(f"Failed to fetch date from {url}: {e}")
         return None
 
+
 # Usage examples
 def get_max_date_json():
     return get_max_date_from_url("https://epaper.eenadu.net/Home/GetMaxdateJson")
 
+
 def get_max_date(name):
     return get_max_date_from_url(f"https://epaper.{name}.com/Login/GetMaxDate", "maxdate")
+
 
 @cache.memoize(timeout=86400)  # Cache for one day
 def get_edition_id(name, target_edition="Khammam"):
@@ -73,7 +78,8 @@ def get_edition_id(name, target_edition="Khammam"):
     else:
         print(f"Error: Unable to fetch data from the URL. Status code: {response.status_code}")
         return None
-    
+
+
 @cache.memoize(timeout=86400)
 def get_khammam_edition(name, supplement=None, target_edition='Khammam'):
     try:
@@ -94,7 +100,7 @@ def get_khammam_edition(name, supplement=None, target_edition='Khammam'):
 
 
 @cache.memoize(timeout=86400)  # Cache for one day
-def get_pages(name,edition_id, max_date):
+def get_pages(name, edition_id, max_date):
     # Adjust the date format if necessary
     formatted_date = datetime.strptime(max_date, '%d/%m/%Y').strftime('%d/%m/%Y')
     url = f"https://epaper.{name}.com/Home/GetAllpages?editionid={edition_id}&editiondate={formatted_date}"
@@ -106,6 +112,7 @@ def get_pages(name,edition_id, max_date):
         print("Failed to fetch Andhrajyothy pages")
         return []
 
+
 def transform_entry(entry, papername):
     transformed_entry = {
         'Path': entry['HighResolution'].replace("\\", "/"),  # Adjust the path format
@@ -115,9 +122,9 @@ def transform_entry(entry, papername):
         'editionID': int(entry['EditionID']),  # Convert EditionID to integer and use it
         'PageId': entry['PageId'],  # Use the same page ID
         'Date': entry['EditionDate'].replace("/", "-"),
-        'Source': papername # Adjust date format if necessary
+        'Source': papername  # Adjust date format if necessary
     }
-    
+
     return transformed_entry
 
 
@@ -127,6 +134,7 @@ def get_eenadu_pages(date, edition_id):
     response = requests.get(url)
     pages = json.loads(response.text)
     return sorted(pages, key=lambda x: int(x['PageNo']))
+
 
 @cache.memoize(timeout=86400)  # Cache for one day
 def get_eenadu_khammam_district_editions(date):
@@ -140,6 +148,7 @@ def get_eenadu_khammam_district_editions(date):
     else:
         print("Failed to fetch district editions")
         return []
+
 
 @cache.memoize(timeout=86400)  # Cache for one day
 def get_editions(date):
@@ -156,14 +165,14 @@ def get_editions(date):
 
     # Get the district editions
     khammam_edition = get_eenadu_khammam_district_editions(date)
-    aj_khammam_edition = get_khammam_edition(name='andhrajyothy',target_edition="Khammam")
-    v_khammam_edition = get_khammam_edition(name='vaartha',target_edition="Khammam")
+    aj_khammam_edition = get_khammam_edition(name='andhrajyothy', target_edition="Khammam")
+    v_khammam_edition = get_khammam_edition(name='vaartha', target_edition="Khammam")
 
-    aprabha_khammam_edition = get_khammam_edition(name='prabhanews',target_edition="Khammam")
-    aprabha_telangana_edition = get_khammam_edition(name='prabhanews',target_edition='Telangana')
+    aprabha_khammam_edition = get_khammam_edition(name='prabhanews', target_edition="Khammam")
+    aprabha_telangana_edition = get_khammam_edition(name='prabhanews', target_edition='Telangana')
 
-    v_khammam_zilla_edition = get_khammam_edition(name='vaartha',supplement=True)
-    a_khammam_zilla_edition = get_khammam_edition(name='andhrajyothy',supplement=True)
+    v_khammam_zilla_edition = get_khammam_edition(name='vaartha', supplement=True)
+    a_khammam_zilla_edition = get_khammam_edition(name='andhrajyothy', supplement=True)
 
     # If Khammam edition is found, add it to the main editions list
     if khammam_edition:
@@ -176,12 +185,13 @@ def get_editions(date):
     if v_khammam_edition:
         editions.append(v_khammam_edition)
         editions.append(v_khammam_zilla_edition)
-    
+
     if aprabha_khammam_edition:
         editions.append(aprabha_khammam_edition)
         editions.append(aprabha_telangana_edition)
-    
+
     return editions
+
 
 @app.route('/', methods=['GET'])
 def landing():
@@ -189,15 +199,16 @@ def landing():
     editions = get_editions(max_date)
     return render_template('landing.html', editions=editions)
 
+
 @app.route('/edition/<string:edition_name>/<int:edition_id>', methods=['GET', 'POST'])
-def edition(edition_name,edition_id):
+def edition(edition_name, edition_id):
     max_date = get_max_date_json()
     if not all_editions:
         get_editions(max_date)
     if edition_name == 'eenadu':
         pages = get_eenadu_pages(max_date, edition_id)
     else:
-        pages = get_pages(name=edition_name,edition_id=edition_id, max_date=get_max_date(name=edition_name))
+        pages = get_pages(name=edition_name, edition_id=edition_id, max_date=get_max_date(name=edition_name))
     if not pages:
         return "No pages found."
     current_page_index = 0
@@ -210,7 +221,9 @@ def edition(edition_name,edition_id):
     current_page = pages[current_page_index]
     xhighres_image_url = current_page['XHighResolution']
     overlay_image_url = xhighres_image_url.replace('.jpg', '.png')
-    return render_template('edition.html', overlay_image_url=overlay_image_url, xhighres_image_url=xhighres_image_url, current_page_index=current_page_index, total_pages=len(pages))
+    return render_template('edition.html', overlay_image_url=overlay_image_url, xhighres_image_url=xhighres_image_url,
+                           current_page_index=current_page_index, total_pages=len(pages))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
